fe17049f03bef0875ce59419d61696074beb4535
ilumsden <lumsden.ian@gmail.com>
2023-06-18 11:06:39 -0700

    Adds some code to init.lua to support the vscode-neovim extension for VSCode

--- init.lua
+++ init.lua
@@ -1,20 +1,28 @@
--- Set leaders
-vim.g.mapleader = ';'
-vim.g.localleader = ';;'
+if vim.g.vscode then
+    local map = vim.api.nvim_set_keymap
+    map('x', 'gc', '<Plug>VSCodeCommentary', {})
+    map('n', 'gc', '<Plug>VSCodeCommentary', {})
+    map('o', 'gc', '<Plug>VSCodeCommentary', {})
+    map('n', 'gcc', '<Plug>VSCodeCommentaryLine', {})
+else
+    -- Set leaders
+    vim.g.mapleader = ';'
+    vim.g.localleader = ';;'
 
--- Set packpath to point to where Packer is installed
-local packer_path = vim.fn.stdpath('config') .. '/site'
-local config_path = vim.fn.stdpath('config')
-local config_files = config_path .. '/?.lua'
-vim.o.packpath = vim.o.packpath .. ',' .. packer_path .. ',' .. config_path
+    -- Set packpath to point to where Packer is installed
+    local packer_path = vim.fn.stdpath('config') .. '/site'
+    local config_path = vim.fn.stdpath('config')
+    local config_files = config_path .. '/?.lua'
+    vim.o.packpath = vim.o.packpath .. ',' .. packer_path .. ',' .. config_path
 
--- Set package.path so the "require" statements below work
-package.path = package.path .. ';' .. config_files
+    -- Set package.path so the "require" statements below work
+    package.path = package.path .. ';' .. config_files
 
--- Split rest of config across multiple files
-require('vars')
-require('opts')
-require('plugs')
-require('user_plugs')
-require('map')
-require('autocmd')
+    -- Split rest of config across multiple files
+    require('vars')
+    require('opts')
+    require('plugs')
+    require('user_plugs')
+    require('map')
+    require('autocmd')
+end
